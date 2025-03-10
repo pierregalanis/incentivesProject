7a8fece7da0b0dfb5efcf53384b4f9a2db365071
nusadown <nusadown@gmail.com>
2025-03-09 20:53:58 -0400

    initializing

---
+++ src/main/java/com/incetiveapi/coreicentives/CoreIcentivesApplication.java
@@ -0,0 +1,13 @@
+package com.incetiveapi.coreicentives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreIcentivesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreIcentivesApplication.class, args);
+    }
+
+}
